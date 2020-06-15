2e7a46f675102fff8a3533c763994059cc0caf64
KYENGHO <ykh6242@gmail.com>
2020-06-16 00:38:44 +0900

    Modified 캐시 soulition

--- Algorithm/캐시/solution.java
+++ Algorithm/캐시/solution.java
@@ -2,56 +2,30 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class solution {
-	// 대소문자 구분 X
-	public int solution1(int cacheSize, String[] cities) {
+	public int solution(int cacheSize, String[] cities) {
+        if (cacheSize == 0) return cities.length * 5;
+        
 		int answer = 0;
 		Queue<String> cache = new LinkedList<String>();
 
 		for (String city : cities) {
 			if (cache.size() < cacheSize) {
-				cache.offer(city);
-				answer += 5;
-			} else if (cache.contains(city)) {
-				cache.remove(city);
-				cache.offer(city);
+				if(cache.contains(city.toLowerCase())) {
+					cache.remove(city.toLowerCase());
+					cache.offer(city.toLowerCase());
+					answer += 1;
+				} else {
+					cache.offer(city.toLowerCase());
+					answer += 5;
+				}
+			} else if (cache.contains(city.toLowerCase())) {
+				cache.remove(city.toLowerCase());
+				cache.offer(city.toLowerCase());
 				answer += 1;
 			} else {
 				cache.poll();
-				cache.offer(city);
+				cache.offer(city.toLowerCase());
 				answer += 5;
-			}
-		}
-
-		return answer;
-	}
-
-	// 대소문자 구분 O
-	public int solution2(int cacheSize, String[] cities) {
-		int answer = 0;
-		Queue<String> cache = new LinkedList<String>();
-
-		for (String city : cities) {
-			if (cache.size() < cacheSize) {
-				cache.offer(city);
-				answer += 5;
-			} else {
-				boolean hit = false;
-				for (int i = 0; i < cache.size(); i++) {
-					String e = cache.poll();
-					cache.offer(e);
-					if (city.equalsIgnoreCase(e)) {
-						hit = true;
-					}
-				}
-				if (hit) {
-					cache.remove(city);
-					cache.offer(city);
-					answer += 1;
-				} else {
-					cache.poll();
-					cache.offer(city);
-					answer += 5;
-				}
 			}
 		}
 

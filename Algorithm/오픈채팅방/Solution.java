77d1195ed1deb937d699ddfa88430fd9981b6b0f
KyeonghoYoo <ykh6242@gmail.com>
2020-09-04 23:06:01 +0900

    Update Solution.java

--- Algorithm/오픈채팅방/Solution.java
+++ Algorithm/오픈채팅방/Solution.java
@@ -27,7 +27,6 @@ class Solution {
 				String nickname = extractedElement[2];
 				userInfoMap.put(userId, nickname);
 			}
-
 		}
 		// logs를 Stream으로 변환한 후, map()을 통해 logs의 요소에 userId를 userInfoMap에 저장된 nickname으로 대체하여 Array로 변환한다.
 		answer = logs.stream().map(e -> {

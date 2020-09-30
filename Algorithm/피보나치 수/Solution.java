b733298dfff6158e401be0f40dc2e9b20627d9ae
KyeonghoYoo <ykh6242@gmail.com>
2020-09-30 18:26:13 +0900

    Update Solution.java

---
+++ Algorithm/피보나치 수/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public  int solution(int n) {
+        int answer = 0;
+        int[] memo = new int[100000];
+        answer = fibo(n, memo);
+        
+        return answer;
+    }
+    
+    public  int fibo(int n, int[] memo){
+        if(n == 1){
+            return 1;
+        } else if(n <= 0){
+            return 0;
+        }
+        if(memo[n - 1] != 0) {
+            return memo[n - 1];
+        }
+        memo[n - 1] = (fibo(n - 2, memo) + fibo(n - 1, memo)) % 1234567;
+
+        return memo[n - 1];
+    }
+}

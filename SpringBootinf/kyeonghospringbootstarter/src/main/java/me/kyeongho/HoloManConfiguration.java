c9c10d9fa8491ca7ed7fea6a7645e995d48a1292
DESKTOP-VS3KH1C\ykh62 <ykh6242@gmail.com>
2020-12-07 12:04:58 +0900

    Configuration을 톨해 사용할 수 있는 Bean을 개발자가 속성들을 정의할 수 있도록 바꿔줌

--- SpringBootinf/kyeonghospringbootstarter/src/main/java/me/kyeongho/HoloManConfiguration.java
+++ SpringBootinf/kyeonghospringbootstarter/src/main/java/me/kyeongho/HoloManConfiguration.java
@@ -1,16 +1,23 @@
 package me.kyeongho;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HoloManConfiguration.class)
 public class HoloManConfiguration {
 
     @Bean
-    public HoloMan holoMan() {
-        HoloMan holoMan = new HoloMan();
-        holoMan.setName("Kyeongho");
-        holoMan.setHowLong(30);
+    @ConditionalOnMissingBean
+    public HoloMan holoMan(HolomanProperties properties) {
+//        HoloMan holoMan = new HoloMan();
+//        holoMan.setName("Kyeongho");
+//        holoMan.setHowLong(30);
+    	HoloMan holoMan = new HoloMan();
+    	holoMan.setName(properties.getName());
+    	holoMan.setHowLong(properties.getHowLong());
 
         return holoMan;
     }
--- SpringBootinf/springbootgettingstarted/src/main/java/me/kyeongho/Application.java
+++ SpringBootinf/springbootgettingstarted/src/main/java/me/kyeongho/Application.java
@@ -3,6 +3,8 @@ package me.kyeongho;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -12,5 +14,16 @@ public class Application {
     	SpringApplication application = new SpringApplication(Application.class);
     	application.setWebApplicationType(WebApplicationType.NONE);
     	application.run(args);
+    }
+    
+    /* SpringBootApplication은 ComponentScan 후 ConfigurationPropertiesScan을 하기 때문에
+     * 개발자가 등록한 Bean이 ConfigurationPropertiesScan에 의해 덮어질 수가 있다.
+     */
+    @Bean
+    public HoloMan holoMan() {
+    	HoloMan holoMan = new HoloMan();
+    	holoMan.setName("ykh");
+    	holoMan.setHowLong(60);
+    	return holoMan;
     }
 }

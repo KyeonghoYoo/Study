749668baa4c4a2b0248c21cdcf4beaf023760b18
KyeonghoYoo <ykh6242@gmail.com>
2020-12-08 13:32:10 +0900

    Spring 외부 설정 학습, 테스트용 외부 설정 방법

--- SpringBootinf/springgettingstartedspringapplication/src/main/java/me/kyeongho/SampleApplicationRunner.java
+++ SpringBootinf/springgettingstartedspringapplication/src/main/java/me/kyeongho/SampleApplicationRunner.java
@@ -1,15 +1,25 @@
 package me.kyeongho;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleApplicationRunner implements ApplicationRunner{
-
+	
+	@Value(value = "${kyeongho.name}")
+	private String name;
+	
+	@Value(value = "${kyeongho.age}")
+	private int age;
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("foo: " + args.containsOption("foo"));
 		System.out.println("bar: " + args.containsOption("bar"));
+		
+		System.out.println("name: " + name);
+		System.out.println("age: " + age);
 	}
 }
--- SpringBootinf/springgettingstartedspringapplication/src/test/java/me/kyeongho/SpringgettingstartedspringapplicationApplicationTests.java
+++ SpringBootinf/springgettingstartedspringapplication/src/test/java/me/kyeongho/SpringgettingstartedspringapplicationApplicationTests.java
@@ -1,13 +1,27 @@
 package me.kyeongho;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@TestPropertySource(properties = "kyeongho.name = Kyeongho Yoo3")
 @SpringBootTest
-class SpringgettingstartedspringapplicationApplicationTests {
+public class SpringgettingstartedspringapplicationApplicationTests {
 
+	@Autowired
+	Environment envrioment;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(envrioment.getProperty("kyeongho.name"))
+		.isEqualTo("Kyeongho Yoo3");
 	}
 
 }

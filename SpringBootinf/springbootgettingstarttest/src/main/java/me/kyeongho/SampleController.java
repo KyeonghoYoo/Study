8c36784891e2fb07f164e84b0bd389501510d299
KyeonghoYoo <ykh6242@gmail.com>
2020-12-10 00:34:22 +0900

    OutputCapture 테스트 유틸 클래스 테스트

--- SpringBootinf/springbootgettingstarttest/src/main/java/me/kyeongho/SampleController.java
+++ SpringBootinf/springbootgettingstarttest/src/main/java/me/kyeongho/SampleController.java
@@ -1,5 +1,7 @@
 package me.kyeongho;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 	
+	Logger log = LoggerFactory.getLogger(SampleController.class);
+
+	
 	@Autowired
 	private SampleService sampleService;
 	
 	@GetMapping("/hello")
 	public String hello() {
+		log.info("log");
+		System.out.println("sysout");
 		return "hello " + sampleService.getName();
 	}
 }
--- SpringBootinf/springbootgettingstarttest/src/test/java/me/kyeongho/ApplicationWebTestClientTests.java
+++ SpringBootinf/springbootgettingstarttest/src/test/java/me/kyeongho/ApplicationWebTestClientTests.java
@@ -1,32 +1,40 @@
 package me.kyeongho;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationWebTestClientTests {
 
+	@Rule
+	public OutputCapture outputCapture = new OutputCapture();
+
 	@Autowired
 	WebTestClient webTestClient;
-	
+
 	@MockBean
 	public SampleService mockSampleService;
 
 	@Test
 	public void test() throws Exception {
 		when(mockSampleService.getName()).thenReturn("ykh");
-		
+
 		webTestClient.get().uri("/hello").exchange()
-		.expectStatus().isOk()
-		.expectBody(String.class).isEqualTo("hello ykh");
+				.expectStatus().isOk().expectBody(String.class)
+				.isEqualTo("hello ykh");
+		assertThat(outputCapture.toString())
+				.contains("log")
+				.contains("sysout");
 	}
 }

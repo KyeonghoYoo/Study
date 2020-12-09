3eacc2f87a5286da95bdef5cf5f4d8658910595e
KyeonghoYoo <ykh6242@gmail.com>
2020-12-09 23:07:26 +0900

    WebEnvironment를 MOCK으로 주고 테스트하는 경우의 코드

--- SpringBootinf/springbootgettingstarttest/src/test/java/me/kyeong/ApplicationTests.java
+++ SpringBootinf/springbootgettingstarttest/src/test/java/me/kyeong/ApplicationTests.java
@@ -1,13 +1,32 @@
 package me.kyeong;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class ApplicationTests {
-
+	
+	@Autowired
+	MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/hello"))
+		.andExpect(status().isOk())
+		.andExpect(content().string("hello Kyeongho"))
+		.andDo(print());
 	}
 
 }

9a2e956b7f0ea414c6f7e7d7ef83c95b31d26fad
KyeonghoYoo <ykh6242@gmail.com>
2020-12-10 15:33:42 +0900

    스프링 부트 Web MVC 프로젝트 생성

---
+++ SpringBootinf/springbootgettingstartwebmvc/src/main/java/me/kyeongho/config/WebConfig.java
@@ -0,0 +1,10 @@
+package me.kyeongho.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebMvc // 스프링 MVC에 대한 설정을 재정의할 때 사용함.
+public class WebConfig implements WebMvcConfigurer {
+
+}

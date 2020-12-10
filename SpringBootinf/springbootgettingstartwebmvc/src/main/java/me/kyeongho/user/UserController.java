f145edfaf2be1d6a8963161a86ef13ac15cba16e
KyeonghoYoo <ykh6242@gmail.com>
2020-12-10 15:56:19 +0900

    HttpMessageConverter가 제공하는 @ReqeustBody와 @ResponseBody에 대한 실습

--- SpringBootinf/springbootgettingstartwebmvc/src/main/java/me/kyeongho/user/UserController.java
+++ SpringBootinf/springbootgettingstartwebmvc/src/main/java/me/kyeongho/user/UserController.java
@@ -1,6 +1,9 @@
 package me.kyeongho.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,10 @@ public class UserController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+	
+	@PostMapping("/users/create")
+	public User create(@RequestBody User user) {
+		return user;
 	}
 }

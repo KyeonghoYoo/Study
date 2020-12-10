e39e2981dbda754a5ba79ebf20ffd60114152b2e
DESKTOP-VS3KH1C\ykh62 <ykh6242@gmail.com>
2020-12-10 11:46:40 +0900

    SpringBoot 배너 banner.txt를 통해 변경

--- SpringBootinf/springgettingstartedspringapplication/src/main/java/me/kyeongho/SpringgettingstartedspringapplicationApplication.java
+++ SpringBootinf/springgettingstartedspringapplication/src/main/java/me/kyeongho/SpringgettingstartedspringapplicationApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringgettingstartedspringapplicationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringgettingstartedspringapplicationApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringgettingstartedspringapplicationApplication.class);
+		app.run(args);
 	}
 
 }
--- SpringBootinf/springgettingstartedspringapplication/src/test/java/me/kyeongho/SpringgettingstartedspringapplicationApplicationTests.java
+++ SpringBootinf/springgettingstartedspringapplication/src/test/java/me/kyeongho/SpringgettingstartedspringapplicationApplicationTests.java
@@ -1,6 +1,6 @@
 package me.kyeongho;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest

8043bd2fbf21c0701404c04ef4db1246f7a74039
KyeonghoYoo <ykh6242@gmail.com>
2020-12-10 14:08:10 +0900

    프로젝트 생성

---
+++ SpringBootinf/springbootgettingstartdevtools/src/main/java/me/kyeong/SpringbootgettingstartdevtoolsApplication.java
@@ -0,0 +1,13 @@
+package me.kyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgettingstartdevtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgettingstartdevtoolsApplication.class, args);
+	}
+
+}
---
+++ SpringBootinf/springbootgettingstartdevtools/src/test/java/me/kyeong/SpringbootgettingstartdevtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootgettingstartdevtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

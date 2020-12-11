d83a73518e55f356c7cb3526e9390c90a1ffda77
KyeonghoYoo <ykh6242@gmail.com>
2020-12-11 13:29:20 +0900

    hateoas를 사용해 응답 JSON에 연관 링크 추가

---
+++ SpringBootinf/springbootgettingstartwebmvc2/src/main/java/me/kyeongho/SampleController.java
@@ -0,0 +1,25 @@
+package me.kyeongho;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping("/hello")
+	public Resource<Hello> hello() {
+		Hello hello = new Hello();
+		hello.setPrefix("Hey, ");
+		hello.setName("Kyeongho");
+		
+		// 연관 링크 추가하는 방법 (여러 방법 중 하나)
+		Resource<Hello> helloResource = new Resource<Hello>(hello);
+		helloResource.add(linkTo(methodOn(SampleController.class).hello()).withSelfRel());
+		
+		return helloResource;
+	}
+}
--- SpringBootinf/springbootgettingstartwebmvc2/src/test/java/me/kyeongho/ApplicationTests.java
+++ SpringBootinf/springbootgettingstartwebmvc2/src/test/java/me/kyeongho/ApplicationTests.java
@@ -1,13 +1,30 @@
 package me.kyeongho;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
 public class ApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		mockMvc.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$._links.self").exists());
 	}
 
 }

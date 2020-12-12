2b69a780f0994baa847dc714df24a74601eb0a2f
KyeonghoYoo <ykh6242@gmail.com>
2020-12-12 19:39:12 +0900

    WebMvcConfigurer를 사용하여 CORS 글로벌 적용

--- SpringBootinf/springbootgettingstartwebmvc/src/main/java/me/kyeongho/config/WebConfig.java
+++ SpringBootinf/springbootgettingstartwebmvc/src/main/java/me/kyeongho/config/WebConfig.java
@@ -1,13 +1,14 @@
 package me.kyeongho.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableWebMvc // 스프링 MVC에 대한 설정을 재정의할 때 사용함.
 public class WebConfig implements WebMvcConfigurer {
-	
+
 	// 아래와 같은 방법으로 정적 리소스 위치를 추가할 수 있음.
 //	@Override
 //	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -15,6 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
 //				.addResourceLocations("classpath:/m/")
 //				.setCachePeriod(20); // 캐싱 전략을 따로 주입해줘야함.
 //	}
-
+	
+	// CORS 글로벌 설정
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:18080");
+	}
 	
 }

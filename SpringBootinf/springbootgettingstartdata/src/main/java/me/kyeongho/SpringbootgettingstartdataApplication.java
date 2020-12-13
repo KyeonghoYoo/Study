3126557bf26434ddc5f63e52f2b9bd6b99f325ff
KyeonghoYoo <ykh6242@gmail.com>
2020-12-13 23:05:34 +0900

    프로젝트 생성

---
+++ SpringBootinf/springbootgettingstartdata/src/main/java/me/kyeongho/SpringbootgettingstartdataApplication.java
@@ -0,0 +1,13 @@
+package me.kyeongho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgettingstartdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgettingstartdataApplication.class, args);
+	}
+
+}

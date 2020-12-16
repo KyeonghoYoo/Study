7a0aa42d89ec11ce3391e6e1a20026bec724f67f
KyeonghoYoo <ykh6242@gmail.com>
2020-12-16 19:02:54 +0900

    프로젝트 생성

---
+++ SpringBootinf/springbootgettingstartdatanosql/src/main/java/me/kyeongho/SpringbootgettingstartdatanosqlApplication.java
@@ -0,0 +1,13 @@
+package me.kyeongho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgettingstartdatanosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgettingstartdatanosqlApplication.class, args);
+	}
+
+}

f7e138651917099a94322dfa045a8baa49620d5d
KyeonghoYoo <ykh6242@gmail.com>
2020-12-16 19:56:37 +0900

    내장형 몽고DB를 사용한 MongoDataTest 단위 테스트 작성

--- SpringBootinf/springbootgettingstartdatanosql/src/main/java/me/kyeongho/mongo/MongoAccountRepository.java
+++ SpringBootinf/springbootgettingstartdatanosql/src/main/java/me/kyeongho/mongo/MongoAccountRepository.java
@@ -1,7 +1,11 @@
 package me.kyeongho.mongo;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MongoAccountRepository extends MongoRepository<MongoAccount, String>{
+
+	Optional<MongoAccount> findByEmail(String id);
 
 }
--- SpringBootinf/springbootgettingstartdatanosql/src/test/java/me/kyeongho/SpringbootgettingstartdatanosqlApplicationTests.java
+++ SpringBootinf/springbootgettingstartdatanosql/src/test/java/me/kyeongho/SpringbootgettingstartdatanosqlApplicationTests.java
@@ -1,13 +1,39 @@
 package me.kyeongho;
 
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import me.kyeongho.mongo.MongoAccount;
+import me.kyeongho.mongo.MongoAccountRepository;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
 public class SpringbootgettingstartdatanosqlApplicationTests {
 
+	@Autowired
+	MongoAccountRepository accountRepository;
+	
 	@Test
 	public void test() {
+		MongoAccount account = new MongoAccount();
+		account.setUserName("kyeongho");
+		account.setEmail("ykh6242@gmail.com");
+		
+		accountRepository.save(account);
+		
+		Optional<MongoAccount> byId = accountRepository.findById(account.getId());
+		assertThat(byId).isNotEmpty();
+		
+		Optional<MongoAccount> ByEmail = accountRepository.findByEmail(account.getEmail());
+		assertThat(ByEmail).isNotEmpty();
+		assertThat(ByEmail.get().getUserName()).isEqualTo("kyeongho");
 		
 	}
 

abeb4820033368ad8bc8b0452b065515c0d051b1
KyeonghoYoo <ykh6242@gmail.com>
2020-12-17 17:32:23 +0900

    Spring Security의 UserDetail 설정 커스터마이징

---
+++ SpringBootinf/springbootgettingstartdsecurity/src/main/java/me/kyeongho/AccountRunner.java
@@ -0,0 +1,28 @@
+package me.kyeongho;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import me.kyeongho.account.Account;
+import me.kyeongho.account.AccountService;
+
+@Component
+public class AccountRunner implements ApplicationRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(AccountRunner.class);
+
+	@Autowired
+	AccountService accountService;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Account kyeongho = accountService.createAccount("Kyeongho", "1234");
+
+		log.info("userName: " + kyeongho.getUserName() + ", password: " + kyeongho.getPassword());
+	}
+
+}
---
+++ SpringBootinf/springbootgettingstartdsecurity/src/main/java/me/kyeongho/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package me.kyeongho.account;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long>{
+
+	Optional<Account> findByUserName(String username);
+
+}

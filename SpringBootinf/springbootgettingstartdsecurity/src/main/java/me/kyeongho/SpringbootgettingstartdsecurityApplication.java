931c1275dc4ad384a9d6d0651d782c97bbf5daf3
KyeonghoYoo <ykh6242@gmail.com>
2020-12-17 11:27:04 +0900

    프로젝트 생성

---
+++ SpringBootinf/springbootgettingstartdsecurity/src/main/java/me/kyeongho/SpringbootgettingstartdsecurityApplication.java
@@ -0,0 +1,13 @@
+package me.kyeongho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgettingstartdsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgettingstartdsecurityApplication.class, args);
+	}
+
+}

1e2a0bc9199e5144f540f29fbb8a77cb2c25c6a1
KyeonghoYoo <ykh6242@gmail.com>
2020-12-18 17:15:47 +0900

    WebClient와 RestTemplate 커스터마이징

--- SpringBootinf/springbootgettingstartwebmvc2/src/main/java/me/kyeongho/Application.java
+++ SpringBootinf/springbootgettingstartwebmvc2/src/main/java/me/kyeongho/Application.java
@@ -2,6 +2,12 @@ package me.kyeongho;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +15,20 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
+	// 전역적으로 WebClient 빌드시 모든 객체에 적용이 된다.
+	@Bean
+	public WebClientCustomizer webClientCustomizer() {
+		return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:18080");
+	}
+	
+	@Bean
+	public RestTemplateCustomizer restTemplateCustomizer() {
+		return new RestTemplateCustomizer() {	
+			@Override
+			public void customize(RestTemplate restTemplate) {
+				restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+			}
+		};
+	}
 }
--- SpringBootinf/springbootgettingstartwebmvc2/src/main/java/me/kyeongho/WebClientRunner.java
+++ SpringBootinf/springbootgettingstartwebmvc2/src/main/java/me/kyeongho/WebClientRunner.java
@@ -17,13 +17,15 @@ public class WebClientRunner implements ApplicationRunner{
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		WebClient webClient = builder.build();
+		WebClient webClient = builder
+				.baseUrl("http://localhost:18080") // 지역적으로 Customizing 하는 법
+				.build();
 		
 		StopWatch stopWatch = new StopWatch("WebClientTest");
 		stopWatch.start("");
 		
 		// TODO /sample1
-		Mono<String> sample1Mono = webClient.get().uri("http://localhost:18080/sample1")
+		Mono<String> sample1Mono = webClient.get().uri("/sample1")
 				.retrieve()
 				.bodyToMono(String.class);
 		
@@ -41,7 +43,7 @@ public class WebClientRunner implements ApplicationRunner{
 		
 		// TODO /sample2
 
-		Mono<String> sample2Mono = webClient.get().uri("http://localhost:18080/sample2")
+		Mono<String> sample2Mono = webClient.get().uri("/sample2")
 				.retrieve()
 				.bodyToMono(String.class);
 		

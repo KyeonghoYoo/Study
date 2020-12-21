81e19b5c24d00431dbf2637e1dbe47567b655faa
KyeonghoYoo <ykh6242@gmail.com>
2020-12-21 22:26:53 +0900

    JPQL 테스트

---
+++ JpaInf/jpagettingstartd/src/main/java/me/kyeongho/JpqlRunner.java
@@ -0,0 +1,49 @@
+package me.kyeongho;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JpqlRunner implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {
+			Member member = new Member();
+			member.setName("Hello");
+			Member member2 = new Member();
+			member2.setName("Hi");
+			// 저장
+			em.persist(member);
+			em.persist(member2);
+			
+			List<Member> result = em.createQuery("select m from Memeber as m", Member.class)
+					.getResultList();
+			
+			for (Member e: result) {
+				System.out.println("memeber.name: " + e.getName());
+			}
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+	}
+}

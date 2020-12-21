7e77606b1cef256a44c01cd063530ee53cd7a03a
KyeonghoYoo <ykh6242@gmail.com>
2020-12-21 20:06:45 +0900

    H2 데이터베이스 컨넥션 테스트

--- JpaInf/jpagettingstartd/src/main/java/me/kyeongho/JpaRunner.java
+++ JpaInf/jpagettingstartd/src/main/java/me/kyeongho/JpaRunner.java
@@ -1,15 +1,25 @@
 package me.kyeongho;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JpaRunner implements ApplicationRunner{
+public class JpaRunner implements ApplicationRunner {
+
+	@Autowired
+	DataSource dataSource;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		
-		
+		try (Connection connection = dataSource.getConnection()) {
+			System.out.println(connection.getMetaData().getURL());
+			System.out.println(connection.getMetaData().getUserName());
+		}
 	}
 }

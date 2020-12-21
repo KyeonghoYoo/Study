9899fb6a57c076790a634d904962c5b73b38c313
KyeonghoYoo <ykh6242@gmail.com>
2020-12-21 21:43:50 +0900

    JPA 저장, 조회, 수정, 삭제 테스트

--- JpaInf/jpagettingstartd/src/main/java/me/kyeongho/JpaRunner.java
+++ JpaInf/jpagettingstartd/src/main/java/me/kyeongho/JpaRunner.java
@@ -26,7 +26,7 @@ public class JpaRunner implements ApplicationRunner {
 			System.out.println(connection.getMetaData().getURL());
 			System.out.println(connection.getMetaData().getUserName());
 		}
-
+		Long id = null;
 		// Unit Name을 넣어서 생성 EntityManagerFactory 생성
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		// 한 트랜잭션에서 실행되는 작업을 시작할 때마다 EntityManager를 반드시 만들어서 사용해줘야함
@@ -41,7 +41,8 @@ public class JpaRunner implements ApplicationRunner {
 			
 			// 저장
 			em.persist(member);
-
+			
+			id = member.getId();
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
@@ -49,6 +50,29 @@ public class JpaRunner implements ApplicationRunner {
 			em.close();
 		}
 
+		EntityManager em2 = emf.createEntityManager();
+		EntityTransaction tx2 = em2.getTransaction();
+		tx2.begin();
+		
+		try {
+			
+			// 조회
+			Member member2 = em2.find(Member.class, id);
+			System.out.println("memeber.id:" + member2.getId());
+			System.out.println("member.name: " + member2.getName());
+			
+			// 수정
+			member2.setName("Hello Kyeongho");
+			
+			//삭제
+			em2.remove(member2);
+			tx2.commit();
+		} catch (Exception e) {
+			tx2.rollback();
+		} finally {
+			em2.close();
+		}
+		
 		emf.close();
 	}
 }

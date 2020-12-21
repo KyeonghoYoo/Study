ec12153aeb43c5dcb8f8c1adcfb9030f6e25dcb4
KyeonghoYoo <ykh6242@gmail.com>
2020-12-21 19:58:55 +0900

    실습 프로젝트 생성

---
+++ JpaInf/jpagettingstartd/src/main/java/me/kyeongho/JpaRunner.java
@@ -0,0 +1,15 @@
+package me.kyeongho;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JpaRunner implements ApplicationRunner{
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		
+	}
+}

e6dbdecd4ce38d12f39733e19e190cc7a544fa74
KyeonghoYoo <ykh6242@gmail.com>
2020-12-23 14:24:57 +0900

    Test 코드 수정

--- JpaInf/jpagettingstartd/src/test/java/me/kyeongho/ApplicationTest.java
+++ JpaInf/jpagettingstartd/src/test/java/me/kyeongho/ApplicationTest.java
@@ -5,16 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.FlushModeType;
 import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
+@DataJdbcTest
 public class ApplicationTest {
 
 	// 동일성 비교 Test
@@ -33,16 +34,16 @@ public class ApplicationTest {
 		em.persist(member);
 
 		tx.commit();
-		
+		System.out.println("------- 동일성 비교 테스트 시작");
 		tx.begin();
-		
-		Member a = em.find(Member.class, "Kyeongho");
-		Member b = em.find(Member.class, "Kyeongho");
 
-		assertThat(a == b).isTrue(); //동일성 비교 true
-		
+		Member a = em.find(Member.class, 1L);
+		Member b = em.find(Member.class, 1L);
+
+		assertThat(a == b).isTrue(); // 동일성 비교 true
+
 		tx.commit();
-		
+
 		em.close();
 
 	}
@@ -54,28 +55,27 @@ public class ApplicationTest {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
+		
+		System.out.println("------- 쓰기 지연 테스트 시작");
 		tx.begin();
 
-		try {
-			Member member1 = new Member();
-			member1.setName("Kyeongho1");
-			Member member2 = new Member();
-			member2.setName("Kyeongho2");
-			
-			// 저장
-			em.persist(member1);
-			System.out.println("1------");
-			em.persist(member2);
-			System.out.println("2------");
-			
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		} finally {
-			em.close();
-		}
+		Member member1 = new Member();
+		member1.setName("Kyeongho1");
+		Member member2 = new Member();
+		member2.setName("Kyeongho2");
+
+		// 저장
+		em.persist(member1);
+		System.out.println("1------");
+		em.persist(member2);
+		System.out.println("2------");
+
+		tx.commit();
+
+		em.close();
+
 	}
-	
+
 	// 변경 감지
 	@Test
 	@Order(3)
@@ -85,17 +85,23 @@ public class ApplicationTest {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 
-		try {
-			Member member = em.find(Member.class, "Kyeongho");
+		Member member1 = new Member();
+		member1.setName("Kyeongho");
+		
+		em.persist(member1);
+		
+		tx.commit();
+		System.out.println("------- 변경 감지 테스트 시작");
+		tx.begin();
+		
+		Member member2 = em.find(Member.class, 1L);
+		
+		member2.setName("ModifiedKyeongho");
+		System.out.println("-------");
 
-			member.setName("ModifiedKyeongho");
-			System.out.println("-------");
-			
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		} finally {
-			em.close();
-		}
+		tx.commit();
+
+		em.close();
+
 	}
 }

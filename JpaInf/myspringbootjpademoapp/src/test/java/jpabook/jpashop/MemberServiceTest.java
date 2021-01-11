c793acd3ec9f84ae15794409b32f5e387aad1b69
KyeonghoYoo <ykh6242@gmail.com>
2021-01-12 00:08:07 +0900

    Test용 properties 생성

--- JpaInf/myspringbootjpademoapp/src/test/java/jpabook/jpashop/MemberServiceTest.java
+++ JpaInf/myspringbootjpademoapp/src/test/java/jpabook/jpashop/MemberServiceTest.java
@@ -1,16 +1,13 @@
 package jpabook.jpashop;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import javax.persistence.EntityManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import jpabook.jpashop.domain.member.Member;

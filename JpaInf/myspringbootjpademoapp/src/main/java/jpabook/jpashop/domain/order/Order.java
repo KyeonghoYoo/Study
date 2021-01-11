fa3c214f70562eb1f62f6156bd94d247e40fbb61
KyeonghoYoo <ykh6242@gmail.com>
2021-01-11 22:47:10 +0900

    Order 엔티티의 양방향 연관관계에 대한 편의 메소드 작성

--- JpaInf/myspringbootjpademoapp/src/main/java/jpabook/jpashop/domain/order/Order.java
+++ JpaInf/myspringbootjpademoapp/src/main/java/jpabook/jpashop/domain/order/Order.java
@@ -42,7 +42,7 @@ public class Order {
 	@OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 
-	@OneToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 
@@ -50,4 +50,20 @@ public class Order {
 
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;
+	
+	// 연관관계 편의 메소드
+	public void setMember(Member member) {
+		this.member = member;
+		member.getOrders().add(this);
+	}
+	
+	public void addOrderItem(OrderItem orderItem) {
+		this.orderItems.add(orderItem);
+		orderItem.setOrder(this);
+	}
+	
+	public void setDelivery(Delivery delivery) {
+		this.delivery = delivery;
+		delivery.setOrder(this);
+	}
 }

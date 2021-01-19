b0744ce0fecacd51cda11a00e9de020e867ce87f
KyeonghoYoo <ykh6242@gmail.com>
2021-01-19 12:55:51 +0900

    프로젝트 생성

---
+++ SpringDataJPAInf/springdatajpademo/src/test/java/me/kyeongho/SpringdatajpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kyeongho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

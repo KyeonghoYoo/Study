3d556e75e6e689085530036746cdb1d3cda675e6
KyeonghoYoo <ykh6242@gmail.com>
2021-01-22 00:25:39 +0900

    주석 오타 수정

--- SpringDataJPAInf/springdatajpademo/src/main/java/me/kyeongho/repository/UsernameOnly.java
+++ SpringDataJPAInf/springdatajpademo/src/main/java/me/kyeongho/repository/UsernameOnly.java
@@ -7,7 +7,7 @@ public interface UsernameOnly {
 	public String getUsername();
 	
 	// Open Projection -> 멤버 엔티티를 모두 조회한 뒤 값을 빼냄.
-	// 장점 -> SPL 지원
+	// 장점 -> SpEL 지원
 	@Value("#{target.username + ' ' + target.age}")
 	public String getBySpl();
 }

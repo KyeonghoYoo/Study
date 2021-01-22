5ce922f6e59ab2fa8236b90f003fd9a3cc6bc144
KyeonghoYoo <ykh6242@gmail.com>
2021-01-22 14:33:23 +0900

    QueryDSL 인프런 강의용 프로젝트 생성

---
+++ QueryDslInf/querydslinfdemo/src/test/java/me/kyeongho/QuerydslinfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kyeongho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslinfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

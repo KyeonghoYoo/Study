b017c759a738484e77b4e0c23e47d0b8d06a3d3f
KyeonghoYoo <ykh6242@gmail.com>
2021-01-22 15:13:49 +0900

    p6spy 의존성 추가 및 설정, QueryDSL 의존성 추가, 플러그인 추가, 간단한 QueryDSL 검증

--- QueryDslInf/querydslinfdemo/src/test/java/me/kyeongho/QuerydslinfdemoApplicationTests.java
+++ QueryDslInf/querydslinfdemo/src/test/java/me/kyeongho/QuerydslinfdemoApplicationTests.java
@@ -1,13 +1,42 @@
 package me.kyeongho;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import me.kyeongho.entity.Hello;
+import me.kyeongho.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslinfdemoApplicationTests {
 
+	@PersistenceContext
+	EntityManager em;
+	
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+		
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+		
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+		
+		System.out.println("result.id = " + hello.getId());
+		
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }

6852e5fdd31e7263eb6791609bb42cbdb38439ed
KyeonghoYoo <ykh6242@gmail.com>
2021-06-23 10:46:36 +0900

    #3 프로젝트 생성
     - 인텔리제이를 통한 프로젝트 생성
     - build.gradle의 기초적인 구조 학습

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹 서비스/my-springboot-webservice/src/test/java/me/kyeongho/myspringbootwebservice/MySpringbootWebserviceApplicationTests.java
@@ -0,0 +1,12 @@
+package me.kyeongho.myspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootWebserviceApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}

380ededbe626bd36e921742f52df988a2270e536
KyeonghoYoo <ykh6242@gmail.com>
2021-07-16 16:14:25 +0900

    #4 Eureka Service 생성, Eureka Client 생성
    - Eureka Discovery 서비스 프로젝트 생성
    - Eureka Client 서비스 프로젝트 생성

---
+++ SpringCloud/kyeongho-discovery-service/src/test/java/me/kyeongho/discovery/KyeonghoDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kyeongho.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyeonghoDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

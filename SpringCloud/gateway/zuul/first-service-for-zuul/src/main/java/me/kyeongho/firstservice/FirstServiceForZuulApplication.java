47285f51a09ecab5e87b39e87f44ba2c9943361d
KyeonghoYoo <ykh6242@gmail.com>
2021-07-16 18:31:31 +0900

    #4 zuul을 통해 Gateway 서비스 구현
    - 디렉토리 정리
    - zuul Gateway 생성
    - Gateway 테스트를 위한 Service 애플리케이션 두 개 생성

---
+++ SpringCloud/gateway/zuul/first-service-for-zuul/src/main/java/me/kyeongho/firstservice/FirstServiceForZuulApplication.java
@@ -0,0 +1,13 @@
+package me.kyeongho.firstservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstServiceForZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstServiceForZuulApplication.class, args);
+	}
+
+}
---
+++ SpringCloud/gateway/zuul/second-service-for-zuul/src/main/java/me/kyeongho/secondservice/SecondServiceForZuulApplication.java
@@ -0,0 +1,13 @@
+package me.kyeongho.secondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondServiceForZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondServiceForZuulApplication.class, args);
+	}
+
+}

1d7b06fde63e768cb6425167f38a129285d12296
KyeonghoYoo <ykh6242@gmail.com>
2022-05-04 18:10:48 +0900

    #4 LoadBalanced 어노테이션 활용
    - Eureka 서버로부터 애플리케이션 이름을 통해 host를 받아와서 요청할 수 있도록 RestTemplate을 설정해줌

--- SpringCloud/service-discovery/user-service/src/main/java/me/kyeongho/userservice/config/web/WebConfiguration.java
+++ SpringCloud/service-discovery/user-service/src/main/java/me/kyeongho/userservice/config/web/WebConfiguration.java
@@ -2,6 +2,7 @@ package me.kyeongho.userservice.config.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ public class WebConfiguration {
      * RestTemplate 빈 등록
      */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
     }

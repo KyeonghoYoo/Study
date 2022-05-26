5c26e834c4789b0384a91b37176c84207f26e9b2
KyeonghoYoo <ykh6242@gmail.com>
2022-05-26 10:14:12 +0900

    #4 catalog-service kafka 접속 정보 수정
    - docker 네트워크 내에 존재하는 카프카 컨테이너로 접속 IP 변경

--- SpringCloud/service-discovery/catalog-service/src/main/java/me/kyeongho/catalogservice/config/messagequeue/KafkaConsumerConfig.java
+++ SpringCloud/service-discovery/catalog-service/src/main/java/me/kyeongho/catalogservice/config/messagequeue/KafkaConsumerConfig.java
@@ -19,7 +19,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         HashMap<String, Object> properties = new HashMap<>();
         /* 카프카 환경설정, 이 또한 외부 설정에서 관리되는 것이 좋다. */
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.0.101:9092");
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId"); // consumer 들을 group화할 수 있다.
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
